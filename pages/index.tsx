c14efbf8474ecaaf136d86a8608fc0b1f2164b7e
aiden-jung <jimmy53120488@gmail.com>
2020-09-14 21:57:59 +0900

    Initial commit
    - create next app에 typescript 추가

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Buzzer Beater</title>
+      </Head>
+      <h1>hello world</h1>
+    </>
+  );
+};
+
+export default Home;
