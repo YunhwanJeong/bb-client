a46bbe8f9f900774aea28e8d9691a7d724f893be
aiden-jung <jimmy53120488@gmail.com>
2020-09-25 13:46:25 +0900

    CHORE: apolloClient 주석 제거

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -6,40 +6,35 @@ import {
   NormalizedCacheObject,
 } from "@apollo/client";
 
+const isServer = () => typeof window === "undefined";
+
 let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
 
-function createApolloClient() {
+const createApolloClient = () => {
   return new ApolloClient({
-    ssrMode: typeof window === "undefined",
+    ssrMode: isServer(),
     link: new HttpLink({
-      uri: "http://localhost:4000/graphql", // Server URL (must be absolute)
-      credentials: "include", // Additional fetch() options like `credentials` or `headers`
+      uri: "http://localhost:4000/graphql",
+      credentials: "include",
     }),
     cache: new InMemoryCache(),
   });
-}
+};
 
-export function initializeApollo(initialState: any = null) {
+export const initializeApollo = (initialState: any = null) => {
   const _apolloClient = apolloClient ?? createApolloClient();
 
-  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
-  // gets hydrated here
   if (initialState) {
-    // Get existing cache, loaded during client side data fetching
     const existingCache = _apolloClient.extract();
-    // Restore the cache using the data passed from getStaticProps/getServerSideProps
-    // combined with the existing cached data
     _apolloClient.cache.restore({ ...existingCache, ...initialState });
   }
-  // For SSG and SSR always create a new Apollo Client
-  if (typeof window === "undefined") return _apolloClient;
-  // Create the Apollo Client once in the client
+  if (isServer) return _apolloClient;
   if (!apolloClient) apolloClient = _apolloClient;
 
   return _apolloClient;
-}
+};
 
-export function useApollo(initialState: any) {
+export const useApollo = (initialState: any) => {
   const store = useMemo(() => initializeApollo(initialState), [initialState]);
   return store;
-}
+};
