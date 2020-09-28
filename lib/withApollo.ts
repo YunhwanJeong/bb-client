97e1011dfc208c204a19959b1216fcd0c91563b6
aiden-jung <jimmy53120488@gmail.com>
2020-09-28 15:24:32 +0900

    FEATURE: client side의 silent refresh 구현
    - client side의 요청 발생 시 accessToken이 존재하지만 만료 시간이 다 된 경우,
    silent refresh를 하도록 로직 추가
    - 위의 silent refresh 도중 refresh token이 유효하지 않아 (e.g 만료 시간이 지난 경우) 에러가 발생하면
    다시 로그인하라는 메시지를 띄울 수 있도록 로직 추가.
    - GraphQL 네트워크 layer에서 발생하는 에러를 콘솔하도록 로직 추가.

--- lib/withApollo.ts
+++ lib/withApollo.ts
@@ -3,15 +3,46 @@ import {
   ApolloLink,
   HttpLink,
   InMemoryCache,
-  from,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { NextPageContext } from "next";
-import { getAccessToken } from "./accessToken";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwt_decode from "jwt-decode";
+import { getAccessToken, setAccessToken } from "./accessToken";
 import createWithApollo from "./createWithApollo";
 
 export const isServer = () => typeof window === "undefined";
 
 const createClient = (ctx: NextPageContext, serverAccessToken: string) => {
+  const tokenRefreshMiddleware = new TokenRefreshLink({
+    accessTokenField: "accessToken",
+    isTokenValidOrUndefined: () => {
+      const token = getAccessToken();
+      if (!token) return true;
+      try {
+        const { exp } = jwt_decode(token);
+        if (Date.now() / 1000 >= exp) {
+          return false;
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    fetchAccessToken: () => {
+      return fetch("http://localhost:4000/auth/refresh-token", {
+        method: "POST",
+        credentials: "include",
+      });
+    },
+    handleFetch: (accessToken) => {
+      setAccessToken(accessToken);
+    },
+    handleError: (err) => {
+      console.warn("Your refresh token is invalid. Please try relogin");
+      console.error(err);
+    },
+  });
   const authMiddleware = new ApolloLink((operation, forward) => {
     const accessToken = isServer() ? serverAccessToken : getAccessToken();
     operation.setContext(({ headers = {} }) => ({
@@ -22,13 +53,23 @@ const createClient = (ctx: NextPageContext, serverAccessToken: string) => {
     }));
     return forward(operation);
   });
+  const errorMiddleware = onError(({ graphQLErrors, networkError }) => {
+    console.error(graphQLErrors);
+    console.error(networkError);
+  });
   const httpLink = new HttpLink({
     uri: "http://localhost:4000/graphql",
     credentials: "include",
   });
+
   return new ApolloClient({
     ssrMode: isServer(),
-    link: from([authMiddleware, httpLink]),
+    link: ApolloLink.from([
+      tokenRefreshMiddleware,
+      authMiddleware,
+      errorMiddleware,
+      httpLink,
+    ]),
     cache: new InMemoryCache(),
   });
 };
