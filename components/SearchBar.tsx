c394c2053c54756c81f263183c3087185d3acdde
aiden-jung <jimmy53120488@gmail.com>
2020-09-29 17:51:09 +0900

    MARKUP: SearchBar 컴포넌트 뼈대 추가

---
+++ components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledSearchBar = styled.div``;
+
+const SearchBar = () => {
+  return (
+    <StyledSearchBar>
+      <img src="../static/svg/icon-search.svg"></img>
+    </StyledSearchBar>
+  );
+};
+
+export default SearchBar;
