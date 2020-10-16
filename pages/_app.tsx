4da973339ff5a4d43643b6a4107553fad28751a4
aiden-jung <jimmy53120488@gmail.com>
2020-10-16 14:46:51 +0900

    FEATURE: _app.tsx에서 로그인 상태를 늘 올바르게 체크, 유지 및 broadcast 하도록 로직 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,73 @@ import { AppProps } from "next/app";
 import GlobalStyle from "../GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { palette } from "../lib/styles";
+import { LoginContext } from "../lib/utils/loginContext";
+import { isServer } from "../lib/utils/isServer";
+import { checkLoginStatus } from "../lib/utils/checkLoginStatus";
+import { useEffect, useState } from "react";
+
+type Profile = {
+  avatar_url?: string;
+};
+export type Me = {
+  id: string;
+  email: string;
+  username: string;
+  profile?: Profile | null;
+};
+export interface LoginState {
+  loading: Boolean;
+  me: Me | null;
+}
+export type SetLoggedIn = (me: Me) => void;
+export type SetLoggedOut = () => void;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [loginState, setLoginState] = useState<LoginState>({
+    loading: true,
+    me: null,
+  });
+  const setLoggedIn = (me: Me) =>
+    setLoginState({
+      loading: false,
+      me,
+    });
+  const setLoggedOut = () =>
+    setLoginState({
+      loading: false,
+      me: null,
+    });
+  useEffect(() => {
+    if (!isServer()) {
+      checkLoginStatus(pageProps?.serverAccessToken)
+        .then((me) => {
+          return setLoginState({
+            loading: false,
+            me,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoginState({
+            loading: false,
+            me: null,
+          });
+        });
+    }
+  }, []);
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={{ palette }}>
-        <Component {...pageProps} />
+        <LoginContext.Provider
+          value={{
+            loginState,
+            setLoggedIn,
+            setLoggedOut,
+          }}
+        >
+          <Component {...pageProps} />
+        </LoginContext.Provider>
       </ThemeProvider>
     </>
   );
